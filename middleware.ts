c1259d5cc9b600e45410620b9caf7a1c403e842a
yrcomerfaruk <omerfarukyericer@gmail.com>
2026-01-02 22:24:59 +0300

    8

--- middleware.ts
+++ middleware.ts
@@ -10,8 +10,8 @@ export async function middleware(request: NextRequest) {
 
   // Create supabase client
   const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://example.supabase.co',
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'example-key',
     {
       cookies: {
         getAll() {
